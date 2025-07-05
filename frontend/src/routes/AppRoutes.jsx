e3390184b6e6a0e48e1a99cb49f9a20283e9d0c4
Ahmed949916 <ahmed40152@gmail.com>
2025-07-05 23:01:40 +0500

    feat:app routes setup

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import RideRequest from "../pages/RideRequest";
+import RideStatus from "../pages/RideStatus";
+import RideHistory from "../pages/RideHistory";
+import DriverDashboard from "../pages/DriverDashboard";
+ 
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/request-ride" element={<RideRequest />} />
+      <Route path="/ride-status" element={<RideStatus />} />
+      <Route path="/ride-history" element={<RideHistory />} />
+      <Route path="/driver" element={<DriverDashboard />} />
+    </Routes>
+  );
+}
